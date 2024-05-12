707a606fc95fe728cfb90e3eb551e720a6a14b50
JurIVoelker <Juri.Voelker@gmx.de>
2024-05-12 20:08:09 +0200

    feat: navbar component

--- components/Navbar/NavBar.js
+++ components/Navbar/NavBar.js
@@ -1,7 +1,35 @@
+import Link from "next/link";
 import styles from "./NavBar.module.scss";
 
 const NavBar = () => {
-  return <div className={styles.navContainer}></div>;
+  return (
+    <div className={styles.navContainer}>
+      <Link href={"/"} className={styles.link}>
+        Start
+      </Link>
+      <Link href={"/"} className={styles.link}>
+        Verein
+      </Link>
+      <Link href={"/"} className={styles.link}>
+        Erwachsene
+      </Link>
+      <Link href={"/"} className={styles.link}>
+        Jugend
+      </Link>
+      <Link href={"/"} className={styles.link}>
+        Galerie
+      </Link>
+      <Link href={"/aktuelles"} className={styles.link}>
+        Aktuelles
+      </Link>
+      <Link href={"/"} className={styles.link}>
+        Verein
+      </Link>
+      <Link href={"/"} className={styles.link}>
+        Anderes
+      </Link>
+    </div>
+  );
 };
 
 export default NavBar;
