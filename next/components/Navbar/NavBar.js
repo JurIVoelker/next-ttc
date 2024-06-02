f1f9c48b276a31ebc4b0328ff8a87c45315327c1
JurIVoelker <Juri.Voelker@gmx.de>
2024-06-02 18:45:09 +0200

    feat: mobile menu

--- next/components/Navbar/NavBar.js
+++ next/components/Navbar/NavBar.js
@@ -1,63 +1,222 @@
 import Link from "next/link";
 import styles from "./NavBar.module.scss";
-import { Button, ListBox, ListBoxItem, Select } from "react-aria-components";
+import {
+  Button,
+  ListBox,
+  ListBoxItem,
+  MenuItem,
+  MenuTrigger,
+  Popover,
+  Menu,
+  Select,
+  SubmenuTrigger,
+} from "react-aria-components";
 import { useFocus, useFocusWithin, useHover } from "react-aria";
 import { useRef, useState } from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowRight,
+  faBars,
+  faChevronRight,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
+
 const NavBar = () => {
-  const vereinLinks = [
-    { name: "Events", href: "/verein/events" },
-    { name: "Organisation", href: "/verein/organisation" },
-    { name: "Sponsoren", href: "/verein/sponsoren" },
-    { name: "Unsere Halle", href: "/verein/halle" },
-  ];
-
-  const erwachseneLinks = [
-    { name: "Training", href: "/erwachsene/training" },
-    { name: "Mannschaften", href: "/erwachsene/mannschaften" },
-  ];
-
-  const jugendLinks = [
-    { name: "Training", href: "/jugend/training" },
-    { name: "Trainer", href: "/jugend/trainer" },
-    { name: "Mannschaften", href: "/jugend/mannschaften" },
-  ];
-
-  const termineLinks = [
-    { name: "Spiele", href: "/termine/spiele" },
+  let items = [
+    { id: "start", name: "Start", href: "/" },
     {
-      name: "Feste und andere Termine",
-      href: "/termine/feste-und-andere-termine",
+      id: "verein",
+      name: "Verein",
+      children: [
+        { id: "events", name: "Events", href: "/verein/events" },
+        {
+          id: "organisation",
+          name: "Organisation",
+          href: "/verein/organisation",
+        },
+        { id: "sponsoren", name: "Sponsoren", href: "/verein/sponsoren" },
+        { id: "halle", name: "Unsere Halle", href: "/verein/halle" },
+      ],
+    },
+    {
+      id: "erwachsene",
+      name: "Erwachsene",
+      children: [
+        { id: "training", name: "Training", href: "/erwachsene/training" },
+        {
+          id: "mannschaften",
+          name: "Mannschaften",
+          href: "/erwachsene/mannschaften",
+        },
+      ],
+    },
+    {
+      id: "jugend",
+      name: "Jugend",
+      children: [
+        { id: "training", name: "Training", href: "/jugend/training" },
+        { id: "trainer", name: "Trainer", href: "/jugend/trainer" },
+        {
+          id: "mannschaften",
+          name: "Mannschaften",
+          href: "/jugend/mannschaften",
+        },
+      ],
+    },
+    {
+      id: "termine",
+      name: "Termine",
+      children: [
+        { id: "spiele", name: "Spiele", href: "/termine/spiele" },
+        {
+          id: "feste",
+          name: "Feste und andere Termine",
+          href: "/termine/feste-und-andere-termine",
+        },
+      ],
+    },
+    { id: "galerie", name: "Galerie", href: "/galerie" },
+    { id: "aktuelles", name: "Aktuelles", href: "/aktuelles" },
+    {
+      id: "anderes",
+      name: "Anderes",
+      children: [
+        { id: "links", name: "Links", href: "/anderes/links" },
+        { id: "downloads", name: "Downloads", href: "/anderes/downloads" },
+        { id: "impressum", name: "Impressum", href: "/anderes/impressum" },
+        {
+          id: "datenschutz",
+          name: "Datenschutz",
+          href: "/anderes/datenschutz",
+        },
+        { id: "kontakt", name: "Kontakt", href: "/anderes/kontakt" },
+      ],
     },
   ];
 
-  const anderesLinks = [
-    { name: "Links", href: "/anderes/links" },
-    {
-      name: "Downloads",
-      href: "/anderes/downloads",
-    },
-    { name: "Impressum", href: "/anderes/impressum" },
-    { name: "Datenschutz", href: "/anderes/datenschutz" },
-    { name: "Kontakt", href: "/anderes/kontakt" },
-  ];
+  const [isMenuVisible, setMenuVisible] = useState(false);
 
   return (
     <div className={styles.navContainer}>
-      <Link href={"/"} className={styles.link}>
-        Start
-      </Link>
-      <LinkSelect options={vereinLinks} label={"Verein"} />
-      <LinkSelect options={erwachseneLinks} label={"Erwachsene"} />
-      <LinkSelect options={jugendLinks} label={"Jugend"} />
-      <LinkSelect options={termineLinks} label={"Termine"} />
-      <Link href={"/galerie"} className={styles.link}>
-        Galerie
-      </Link>
-      <Link href={"/aktuelles"} className={styles.link}>
-        Aktuelles
-      </Link>
-      <LinkSelect options={anderesLinks} label={"Anderes"} />
+      <MenuTrigger isOpen={isMenuVisible} onOpenChange={setMenuVisible}>
+        <div className={styles.buttonContainer}>
+          <Button aria-label="Menü">
+            <FontAwesomeIcon icon={faBars} />
+          </Button>
+        </div>
+        <Popover className={styles.popover}>
+          <Button
+            className={styles.exitButton}
+            aria-label="Menü schließen"
+            onPress={() => {
+              setMenuVisible(false);
+            }}
+          >
+            <FontAwesomeIcon icon={faXmark} />
+          </Button>
+          <div className={styles.menuWrapper}>
+            <Menu items={items} className={styles.menu}>
+              {function renderSubmenu(item) {
+                if (item.children) {
+                  return (
+                    <SubmenuTrigger>
+                      <MenuItem
+                        className={styles.menuItem}
+                        key={item.name}
+                        href={item.href && item.href}
+                      >
+                        {item.name}
+                        <FontAwesomeIcon icon={faChevronRight} />
+                      </MenuItem>
+                      <Popover>
+                        <Menu
+                          items={item.children}
+                          className={`${styles.menu} ${styles.submenu}`}
+                        >
+                          {(item) => renderSubmenu(item)}
+                        </Menu>
+                      </Popover>
+                    </SubmenuTrigger>
+                  );
+                } else {
+                  return (
+                    <MenuItem
+                      className={styles.menuItem}
+                      key={item.name}
+                      href={item.href && item.href}
+                    >
+                      {item.name}
+                    </MenuItem>
+                  );
+                }
+              }}
+            </Menu>
+          </div>
+        </Popover>
+      </MenuTrigger>
+      {/* 
+        <div className={styles.hamburgerButtonWrapper}>
+          <Button
+            className={styles.hamburgerButton}
+            onPress={handleMenuVisible}
+          >
+            <FontAwesomeIcon icon={faBars} />
+          </Button>
+        </div>
+        <div className={`${styles.linkContainer}`}>
+          <Link href={"/"} className={styles.link}>
+            Start
+          </Link>
+          <LinkSelect options={vereinLinks} label={"Verein"} />
+          <LinkSelect options={erwachseneLinks} label={"Erwachsene"} />
+          <LinkSelect options={jugendLinks} label={"Jugend"} />
+          <LinkSelect options={termineLinks} label={"Termine"} />
+          <Link href={"/galerie"} className={styles.link}>
+            Galerie
+          </Link>
+          <Link href={"/aktuelles"} className={styles.link}>
+            Aktuelles
+          </Link>
+          <LinkSelect options={anderesLinks} label={"Anderes"} />
+        </div>
+
+        <Popover
+          className={`${
+            isMenuVisible ? styles.menuVisible : styles.menuHidden
+          }`}
+        >
+          <Menu>
+            <MenuItem href="/">Start</MenuItem>
+            <SubmenuTrigger>
+              <MenuItem>Verein</MenuItem>
+              <Popover>
+                {vereinLinks.map((link) => (
+                  <MenuItem id={link.name}>{link.name}</MenuItem>
+                ))}
+              </Popover>
+            </SubmenuTrigger>
+          </Menu>
+        </Popover> */}
+      {/* <div
+        className={`${isMenuVisible ? styles.menuVisible : styles.menuHidden}`}
+      >
+        <Link href={"/"} className={styles.link}>
+          Start
+        </Link>
+        <MobileLinks options={vereinLinks} label={"Verein"} />
+        <MobileLinks options={erwachseneLinks} label={"Erwachsene"} />
+        <MobileLinks options={jugendLinks} label={"Jugend"} />
+        <MobileLinks options={termineLinks} label={"Termine"} />
+        <Link href={"/galerie"} className={styles.link}>
+          Galerie
+        </Link>
+        <Link href={"/aktuelles"} className={styles.link}>
+          Aktuelles
+        </Link>
+        <MobileLinks options={anderesLinks} label={"Anderes"} />
+      </div> */}
+      {/* </div> */}
     </div>
   );
 };
@@ -127,6 +286,18 @@ const LinkSelect = ({ options, label }) => {
           </div>
         </Select>
       </span>
+    </>
+  );
+};
+const MobileLinks = ({ options, label }) => {
+  return (
+    <>
+      {label}
+      <div className={styles.mobileLinks}>
+        {options.map((option) => (
+          <Link href={option.href}>{option.name}</Link>
+        ))}
+      </div>
     </>
   );
 };
