c6d43d8c552abddc47899eabc7a408434bfd42b5
JurIVoelker <Juri.Voelker@gmx.de>
2024-06-04 22:31:34 +0200

    feat: footer with logos

--- next/components/Footer/Footer.js
+++ next/components/Footer/Footer.js
@@ -1,10 +1,23 @@
 import Link from "next/link";
 import styles from "./Footer.module.scss";
+import Image from "next/image";
 
 function Footer() {
   return (
     <div className={styles.footer}>
-      <Link href={"/login"}>Login</Link>
+      <div className={styles.logos}>
+        <Image
+          src={"http://127.0.0.1:1337/uploads/logo_ttc_farbig_ac89ff5756.png"}
+          width={500}
+          height={130}
+        />
+        <Image
+          src={"http://127.0.0.1:1337/uploads/logo_joola_20aaf9f6f4.png"}
+          width={500}
+          height={130}
+        />
+      </div>
+      <Link href={"/login"}>© 2023 TTC Klingenmünster </Link>
     </div>
   );
 }
