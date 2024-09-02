434ead630b1c13758d8d0eaa2da826dac9c74f97
JurIVoelker <Juri.Voelker@gmx.de>
2024-09-02 13:55:16 +0200

    refactor(backend): :fire: remove unused code

--- frontend/pages/api/download-team-line-up.js
+++
@@ -1,6 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method !== "POST")
-    return res.status(404).json({ message: "not found" });
-
-  res.status(200).send(mainPlayers);
-}
