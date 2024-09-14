4323b93da69b7e49d35f15e9bd6dee7904e32ed4
JurIVoelker <Juri.Voelker@gmx.de>
2024-09-14 11:23:22 +0200

    chore(deployment): test gh webhook

--- frontend/pages/api/send-confirmation-mail.js
+++ frontend/pages/api/send-confirmation-mail.js
@@ -36,7 +36,7 @@ export default async function handler(req, res) {
       html: emailHtml,
     });
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return res.status(500).json({ message: "Error" });
   }
   res.status(200).json({ message: "mail was successfully sent" });
