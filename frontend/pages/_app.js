2e2d8c4a83fc8471eb54f911abae378c1c4589ee
JurIVoelker <Juri.Voelker@gmx.de>
2024-09-16 20:47:12 +0200

    fix: fetch route

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 import App from "next/app";
 import axios from "axios";
-import { getStrapiImage } from "../utils/strapi";
+import { getStrapiImage, publicStrapiUrl } from "../utils/strapi";
 import Layout from "../components/Layout/Layout";
 import Seo from "../components/Seo/Seo";
 export default function MyApp({ Component, pageProps, globalFooterData }) {
@@ -26,8 +26,9 @@ export default function MyApp({ Component, pageProps, globalFooterData }) {
 MyApp.getInitialProps = async (appContext) => {
   const appProps = await App.getInitialProps(appContext);
   try {
-    const strapiUrl = process.env.STRAPI_URL_LOCAL;
-    const response = await axios.get(`${strapiUrl}/api/global?populate=*`);
+    const response = await axios.get(
+      `${publicStrapiUrl}/api/global?populate=*`
+    );
     const { joolaLogo, ttcKlingenmuensterLogo } = response.data.data.attributes;
     return {
       ...appProps,
