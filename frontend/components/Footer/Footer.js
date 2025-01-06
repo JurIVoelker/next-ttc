640f01cf0232bf0cdfeb43395ab483fb378f0b62
JurIVoelker <Juri.Voelker@gmx.de>
2025-01-06 18:52:17 +0100

    fix: update footer to display the current year dynamically

--- frontend/components/Footer/Footer.js
+++ frontend/components/Footer/Footer.js
@@ -3,13 +3,14 @@ import styles from "./Footer.module.scss";
 
 function Footer({ global }) {
   const { joolaLogoURL, ttcKlingenmuensterLogoURL } = global;
+  const currentYear = new Date().getFullYear();
   return (
     <div className={styles.footer}>
       <div className={styles.logos}>
         <img src={ttcKlingenmuensterLogoURL} width={500} height={130} />
         <img src={joolaLogoURL} width={500} height={130} />
       </div>
-      <Link href={"/login"}>© 2023 TTC Klingenmünster </Link>
+      <Link href={"/login"}>{`© ${currentYear} TTC Klingenmünster`}</Link>
     </div>
   );
 }
