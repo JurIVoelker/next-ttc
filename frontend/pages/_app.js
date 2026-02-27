03e38c504a4996e2264daab42fa03ff18533c660
JurIVoelker <juri.voelker.de>
2026-02-27 15:54:39 +0100

    feat: remove jwt from localstorage when jwt is expired

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -4,10 +4,23 @@ import axios from "axios";
 import { getStrapiImage, publicStrapiUrl } from "../utils/strapi";
 import Layout from "../components/Layout/Layout";
 import Seo from "../components/Seo/Seo";
+import { useEffect } from "react";
+import { decode } from "jsonwebtoken";
+
 export default function MyApp({ Component, pageProps, globalFooterData }) {
   const seo = pageProps?.strapiData?.data?.attributes?.seo;
   const aktuellesTitle = pageProps?.postData?.attributes?.titel;
   const aktuellesDescription = pageProps?.postData?.attributes?.text;
+
+  useEffect(() => {
+    if (typeof localStorage === "undefined") return;
+    const jwt = localStorage.getItem("jwt");
+    if (!jwt) return;
+    const jwtDecoded = decode(jwt);
+    if (jwtDecoded.exp * 1000 < Date.now()) {
+      localStorage.removeItem("jwt");
+    }
+  }, []);
 
   return (
     <>
@@ -27,7 +40,7 @@ MyApp.getInitialProps = async (appContext) => {
   const appProps = await App.getInitialProps(appContext);
   try {
     const response = await axios.get(
-      `${publicStrapiUrl}/api/global?populate=*`
+      `${publicStrapiUrl}/api/global?populate=*`,
     );
     const { joolaLogo, ttcKlingenmuensterLogo } = response.data.data.attributes;
     return {
